39efc6ca66a1845db50c7be0a03fe3836ff6bd90
Grace Gamble <grace.h.gamble@gmail.com>
2019-03-01 10:09:50 -0700

    Different Ways of Handling Events, Including Controlled and Uncontrolled with State

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,25 @@
 import React, {Component} from 'react';
 
 class SearchBar extends Component {
+      // onInputChange(event) {
+      //       console.log(event.target.value);
+      // }
+      state = {
+            term: ''
+      };
       render() {
             return (
                   <div className="ui segment">
                         <form className="ui form">
                               <div className="field">
                                     <label>Image Search</label>
-                                    <input type="text" />
+                                    <input
+                                          type="text"
+                                          // onChange={(e) => console.log(e.target.value)}
+                                          // onChange={this.onInputChange}
+                                          value={this.state.term}
+                                          onChange={(e) => this.setState({term: e.target.value})}
+                                    />
                               </div>
                         </form>
                   </div>
