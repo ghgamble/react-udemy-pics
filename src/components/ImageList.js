4d36b7c4d38199c03ed3bd33caa1e91673a889b6
Grace Gamble <grace.h.gamble@gmail.com>
2019-03-05 11:53:35 -0700

    Complete and Refactored List of Images

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,8 +1,15 @@
-import React, {Component} from 'react';
+import React from 'react';
 
-const ImageList = () => {
+const ImageList = (props) => {
+      const images = props.images.map((image) => {
+            return <img
+                  src={image.urls.regular}
+                  key={image.id}
+                  alt={image.description}
+            />
+      });
       return (
-            <div>ImageList</div>
+            <div>{images}</div>
       );
 }
 
