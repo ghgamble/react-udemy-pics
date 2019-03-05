608ab61691123063b2eb5e24d47c7f1ecaf86e75
Grace Gamble <grace.h.gamble@gmail.com>
2019-03-05 10:55:46 -0700

    Set Up Axios, Get Request for Query Image Search

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,18 @@
 import React, {Component} from 'react';
+import axios from 'axios';
 
 import SearchBar from './SearchBar';
-import ImageList from './ImageList';
+// import ImageList from './ImageList';
 
 class App extends Component {
       onSearchSubmit(term) {
-            console.log(term);
+            // console.log(term);
+            axios.get('https://api.unsplash.com/search/photos', {
+                  params: { query: term },
+                  headers: {
+                        Authorization: 'Client-ID f54fc9fe0b91157a7522878f6d88efa206cc3d897836d08b2dafcc36615f4ca5'
+                  }
+            });
       }
       render() {
             return (
