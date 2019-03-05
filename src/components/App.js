5769b4666ea60eac968d05dda411731c883087e8
Grace Gamble <grace.h.gamble@gmail.com>
2019-03-05 11:02:40 -0700

    Axios Async Await

--- src/components/App.js
+++ src/components/App.js
@@ -5,14 +5,15 @@ import SearchBar from './SearchBar';
 // import ImageList from './ImageList';
 
 class App extends Component {
-      onSearchSubmit(term) {
+      async onSearchSubmit(term) {
             // console.log(term);
-            axios.get('https://api.unsplash.com/search/photos', {
+            const response = await axios.get('https://api.unsplash.com/search/photos', {
                   params: { query: term },
                   headers: {
                         Authorization: 'Client-ID f54fc9fe0b91157a7522878f6d88efa206cc3d897836d08b2dafcc36615f4ca5'
                   }
             });
+            console.log(response.data.results);
       }
       render() {
             return (
