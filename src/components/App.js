89df49ed710ef869e8b05d753e490a06b7d3b075
Grace Gamble <grace.h.gamble@gmail.com>
2019-03-05 11:19:46 -0700

    Fix 'this' Issue with Fat Arrow Function

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,10 @@ import SearchBar from './SearchBar';
 // import ImageList from './ImageList';
 
 class App extends Component {
-      async onSearchSubmit(term) {
+      state = {
+            images: []
+      };
+      onSearchSubmit = async (term) => {
             // console.log(term);
             const response = await axios.get('https://api.unsplash.com/search/photos', {
                   params: { query: term },
@@ -13,12 +16,13 @@ class App extends Component {
                         Authorization: 'Client-ID f54fc9fe0b91157a7522878f6d88efa206cc3d897836d08b2dafcc36615f4ca5'
                   }
             });
-            console.log(response.data.results);
+            this.setState({ images: response.data.results });
       }
       render() {
             return (
                   <div className="ui container" style={{ marginTop: '10px' }}>
                         <SearchBar onSubmit={this.onSearchSubmit} />
+                        Found: {this.state.images.length} images
                   </div>
             );
       }
