19ef360539c42dce7c4dbd58650c149c178e2e84
Grace Gamble <grace.h.gamble@gmail.com>
2019-03-11 10:58:04 -0600

    Create Image Card Component for CSS

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './ImageList.css';
+import ImageCard from './ImageCard';
 
 const ImageList = (props) => {
       const images = props.images.map((image) => {
-            return <img
-                  src={image.urls.regular}
+            return <ImageCard
                   key={image.id}
-                  alt={image.description}
+                  image={image}
             />
       });
       return (
